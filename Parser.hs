5b4ddcc1100af4959690b94594377d78ebca0a80
Paul Freeman <freemapa@onid.orst.edu>
2013-02-16 20:44:23 -0800

    Non-working version -- needs attention

--- Parser.hs
+++ Parser.hs
@@ -1,3 +1,34 @@
 import Scanner
+import Text.ParserCombinators.Parsec
+
+type ourParser a :: GenParser Token () a
+
+mytoken :: Show t => t -> GenParser ((Int,Int),t) () t
+mytoken x
+  = token showTok posFromTok testTok
+  where
+    showTok t           = show t
+    posFromTok t        = 0
+    testTok t           = if (x == t) then Just t else Nothing
+
+parseLeftParen :: ourParse String
+parseLeftParen 
+  = mytoken (\tok -> case tok of 
+                       LeftParen  -> Just "("
+                       other      -> Nothing)
+
+parseRightParen :: ourParse String
+parseRightParen 
+  = mytoken (\tok -> case tok of 
+                       RightParen -> Just ")"
+                       other      -> Nothing)
+
+parens  :: Parser ()
+parens  = do{ parseLeftParen
+            ; parens
+            ; parseRightParen
+            ; parens
+            }
+        <|> return ()
 
 main = do putStrLn "Hello"
