104c015ba9f95a7dff4f84a8e7907d36fd462d56
Paul Freeman <freemapa@onid.orst.edu>
2013-03-04 20:00:56 -0800

    Changes made with Kevin to add tree.

--- Parser.hs
+++ Parser.hs
@@ -5,6 +5,7 @@ import Scanner
 import Text.Parsec
 import Text.Parsec.Pos
 import Text.Parsec.String
+import Data.Tree
 
 help = "Usage:\n         Parse [option] [files]\n\n"++
             "-h, --help  -> This usage document.\n"++
@@ -15,45 +16,49 @@ type OurParser a b = GenParser Token a b
 
 {- Grammar -}
 -- F -> TF | <EOF>
-f = do{ x<-t; y<-f; return (x++y) }
+f = do{ x<-t;
+        y<-f;
+        return (Node "$" [x,y]) }
     <|> do{ x<-parseEOF <?> "end of file"; return x }
 -- T -> (S)
-t = do{ x<-parseLeftParen <?> "("; updateState(4+); y<-s; updateState(subtract 4);
-        z<-parseRightParen <?> ")"; return (x++y++z) }
+t = do{ x<-parseLeftParen <?> "("; updateState(1+); y<-s; updateState(subtract 1);
+        z<-parseRightParen <?> ")"; return y }
 -- S -> (A | atomB
-s = do{ x<-parseLeftParen <?> "("; updateState(4+); y<-a; return (x++y) }
-    <|> do{ x<-parseAtom <?> "atom"; y<-b; return (x++y) }
+s = do{ x<-parseLeftParen <?> "("; updateState(1+); y<-a; return y }
+    <|> do{ x<-parseAtom <?> "atom";
+            y<-b;
+            return $ x {subForest = subForest(x)++[y]} }
 -- A -> )B | S)B
-a = do{ updateState(subtract 4); x<-parseRightParen <?> ")"; y<-b; return (x++y) }
-    <|> do{ x<-s; updateState(subtract 4); y<-parseRightParen <?> ")"; z<-b; return (x++y++z) }
+a = do{ updateState(subtract 1); x<-parseRightParen <?> ")"; y<-b; return y }
+    <|> do{ x<-s; updateState(subtract 1); y<-parseRightParen <?> ")"; z<-b; return (Node "A" [x,z])  }
 -- B -> S | Empty
-b = do{x<-s; return x}
-    <|> return "" -- epsilon
+b = do{x<-s; return x }
+    <|> return (Node "e" [])   -- epsilon
 
 {- Parsers -}
 parseLeftParen = do
     i <- getState
-    mytoken (\t -> case t of LeftParen  -> Just(indent i++"(\n")
+    mytoken (\t -> case t of LeftParen  -> Just(Node "(" [] )
                              other      -> Nothing)
 parseRightParen = do
     i <- getState
-    mytoken (\t -> case t of RightParen -> Just(indent i++")\n")
+    mytoken (\t -> case t of RightParen -> Just(Node ")" [] )
                              other      -> Nothing)
 parseAtom = do
     i <- getState
     mytoken (\t -> case t of EOF        -> Nothing
                              LeftParen  -> Nothing
                              RightParen -> Nothing
-                             other      -> Just(indent i++show t++"\n"))
+                             other      -> Just(Node (show t) []))
 parseEOF = do
-    mytoken (\t -> case t of EOF        -> Just"<EOF>\n"
+    mytoken (\t -> case t of EOF        -> Just(Node "<EOF>" [])
                              other      -> Nothing)
 {- Helpers -}
 mytoken test = tokenPrim show update_pos test
 
 update_pos pos _ _ = newPos "" 0 0
 
-indent n = take n (repeat ' ')
+indent n = take (4*n) (repeat ' ')
 
 {- main -}
 main = do
@@ -71,4 +76,4 @@ main = do
                     contents <- readFile file
                     case (runParser f 0 file $ lexer contents) of
                         Left err -> print err
-                        Right xs -> putStr xs
+                        Right xs -> putStr (drawTree xs)
